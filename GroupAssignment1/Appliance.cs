bead3b47a3f38d171e9e156c7bc279ac83ddd094
Hans Feliciano <hgvfeliciano10@gmail.com>
2025-02-05 13:26:06 -0700

    Add project files.

---
+++ GroupAssignment1/Appliance.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupAssignment1
+{
+    internal abstract class Appliance
+    {
+
+        // private fields
+        private string _brand, _color, itemNumber;
+        private double _price, _wattage;
+        private int _quantity;
+
+        // public properties
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public string ItemNumber { get; set; }
+        public double Price { get; set; }
+
+        public double Wattage { get; set; } 
+        public int Quantity { get; set; }
+
+        // Methods 
+        public Appliance(string brand, string color, string itemNumber, double price, int quantity, double wattage)
+        {
+            this.Brand = brand;
+            this.Color = color;
+            this.ItemNumber = itemNumber;
+            this.Price = price;
+            this.Quantity = quantity;
+            this.Wattage = wattage;
+        }
+
+        public abstract void Checkout();
+        public abstract void DetermineAppliance();
+        public abstract void FormatForFile();
+
+    } // abstract class
+} // namespace
