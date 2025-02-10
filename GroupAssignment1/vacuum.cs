fda6401cb6681aa66b1922e273c19bc5196d4dda
enzo <enzo.campanatorres@edu.sait.ca>
2025-02-09 20:49:04 -0700

    class

--- GroupAssignment1/vacuum.cs
+++ GroupAssignment1/vacuum.cs
@@ -6,7 +6,55 @@ using System.Threading.Tasks;
 
 namespace GroupAssignment1
 {
-    internal class vacuum
+    internal class Vacuum : Appliance
     {
+        private int _batteryVoltage;
+        private int _grade;
+
+        public int BatteryVoltage
+        {
+            get { return _batteryVoltage; }
+            set { _batteryVoltage = value >= 0 ? value:0 ; }
+        }
+
+        public int Grade
+        {
+            get { return _grade; }
+            set { _grade = value >= 0 ? value : 0; }
+        }
+
+        public Vacuum(string itemNumber, string brand, int quantity, double wattage, string color, double price,int batteryVoltage, int grade) :
+            base(itemNumber,brand, quantity, wattage, color, price)
+        {
+            this.BatteryVoltage = batteryVoltage;
+            this.Grade = grade;
+        }
+
+        public override void Checkout()
+        {
+            
+        }
+
+        public override void DetermineAppliance()
+        {
+
+        }
+
+        public override string FormatForFile()
+        {
+            return $"{ItemNumber},{Brand},{Quantity},{Wattage},{Color},{Grade},{BatteryVoltage}";
+        }
+
+        public override string ToString()
+        {
+            return $"Item Number: {ItemNumber}\n" +
+                   $"Brand: {Brand}\n" +
+                   $"Wattage: {Quantity}\n" +
+                   $"Color: {Color}\n" +
+                   $"Price: {Price}\n" +
+                   $"Grade: {Grade}\n" +
+                   $"Battery Voltage:{BatteryVoltage}\n"
+                ;
+        }
     }
 }
