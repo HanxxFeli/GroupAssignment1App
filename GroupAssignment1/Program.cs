1cc2b3a61205ee0c2a7a9663d6ef733ae381c5dd
Hans Feliciano <hgvfeliciano10@gmail.com>
2025-02-11 10:43:04 -0700

    test

--- GroupAssignment1/Program.cs
+++ GroupAssignment1/Program.cs
@@ -1,4 +1,55 @@
 ﻿using GroupAssignment1;
-Appliance frige = new Refrigerator("232399432", "Tofu", 34455, 120, "Black", 12345, 2, 34, 34);
-frige.Checkout();
-Console.WriteLine(frige.Quantity);
+
+Microwave testMicrowave = new Microwave("302618394", "Hoover", 159, 100, "white", 99.99, 1.1, roomType.K);
+
+Console.WriteLine(testMicrowave.RoomTypeDisplay);
+
+Console.WriteLine("Welcome to Modern Appliances!" +
+    "\nHow May We Assist You?" +
+    "\n1 – Check out appliance" +
+    "\n2 – Find appliances by brand" +
+    "\n3 – Display appliances by type" +
+    "\n4 – Produce random appliance list" +
+    "\n5 – Save & exit\n");
+
+Console.WriteLine("Enter option:");
+string option = Console.ReadLine();
+int userOption = Convert.ToInt32(option);
+
+
+switch (userOption)
+{
+    case 1:
+        // appliance can be checked out 
+
+        // appliance not available 
+
+        // appliance does not exist
+        break;
+    case 2:
+        // search for appliance using appliance.brand 
+
+        break;
+    case 3:
+        // display appliance types 
+        Console.WriteLine("Appliance Types" +
+            "\n1 – Refrigerators" +
+            "\n2 – Vacuums" +
+            "\n3 – Microwaves" +
+            "\n4 – Dishwashers\n");
+
+        Console.WriteLine("Enter type of appliance:");
+        string applianceOption = Console.ReadLine();
+
+        break;
+    case 4:
+        Console.WriteLine("Enter number of appliances:");
+        string numOfAppliance = Console.ReadLine();
+
+
+        break;
+    case 5:
+
+
+        break;
+}
